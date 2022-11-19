dd1e0516b4818162a73ccdb4e89a745004c15cc0
gaoxinge <gaoxx5@gmail.com>
2022-11-19 20:12:03 +0800

    docs

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,12 @@ export default defineComponent({
   <a-layout class="layout">
     <a-layout-header :style="{height: '100px'}">
       <a-menu v-model:selectedKeys="selectedKeys" theme="dark" mode="horizontal" :style="{lineHeight: '100px', left: '60px', fontSize: '18px'}">
-          <a-menu-item key="1" :style="{left: '50px', background: '#001529'}"><RouterLink to="/timeline">时间线</RouterLink></a-menu-item>
-          <a-menu-item key="2" :style="{left: '100px', background: '#001529'}"><RouterLink to="/tag">标签</RouterLink></a-menu-item>
+          <a-menu-item key="1" :style="{left: '50px', background: '#001529'}"><RouterLink to="/">时间线</RouterLink></a-menu-item>
+          <a-menu-item key="2" :style="{left: '100px', background: '#001529'}"><RouterLink to="/sql">SQL</RouterLink></a-menu-item>
       </a-menu>
     </a-layout-header>
     <a-layout-content :style="{width: '60%', marginTop: '100px', marginLeft: 'auto', marginRight: 'auto', fontSize: '18px'}">
-      <RouterView />
+      <RouterView name="app" />
     </a-layout-content>
   </a-layout>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,37 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TimelineVue from '@/components/timeline/Timeline.vue'
+import TimelineVue from '@/components/Timeline.vue'
+import SQLVue from '@/components/Test.vue'
 import MarkdownVue from '@/shared/Markdown.vue'
 import blog0 from '@/blog/0.md?raw'
 import blog1 from '@/blog/1.md?raw'
 
-const metas: Record<string, string>[] = [
-  { date: "2022-11-18", title: "HELLO WORLD", md: blog0, description: "" },
-  { date: "2022-11-18", title: "LATEX", md: blog1, description: "" }
+const metas = [
+  { date: "2022-11-18", title: "HELLO WORLD", md: blog0, tags: [], description: "" },
+  { date: "2022-11-18", title: "LATEX", md: blog1, tags: [], description: "",  }
 ];
 
-for (let i = 0; i < metas.length; i++) {
-  metas[i].href = `/blog${i}`;
+let listData: Record<string, string>[] = [];
+for (let i = metas.length - 1; i > -1; i--) {
+  listData.push({
+    date: metas[i].date,
+    title: metas[i].title,
+    description: metas[i].description,
+    href: `/blog${i}`,
+  });
 }
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/timeline',
+      path: '/',
       name: 'timeline',
-      component: TimelineVue,
+      components: {
+        app: TimelineVue
+      },
       props: {
-        listData: metas,
+        app: {
+          listData: listData
+        },
       }
     },
     {
-      path: '/tag',
-      name: 'tag',
-      component: TimelineVue,
-      props: {
-        listData: metas,
+      path: '/sql',
+      name: 'sql',
+      components: {
+        app: SQLVue
       }
-    },
+    }
   ]
 })
 
@@ -39,10 +49,14 @@ for (let i = 0; i < metas.length; i++) {
   router.addRoute({
     path: `/blog${i}`,
     name: `blog${i}`,
-    component: MarkdownVue,
+    components: {
+      app: MarkdownVue
+    },
     props: {
-      title: metas[i].title,
-      md: metas[i].md,
+      app: {
+        title: metas[i].title,
+        md: metas[i].md,
+      },
     }
   });
 }
